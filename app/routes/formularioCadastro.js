2fd4f7d7288b81183be4eeeded97294bfeb9cb39
Jorge Avila <jorgeavila11@hotmail.com>
2022-04-09 15:16:54 -0300

    versao 4.0 inserindo dados no formulario

--- app/routes/formularioCadastro.js
+++ app/routes/formularioCadastro.js
@@ -1,11 +1,22 @@
+var dbConnection = require('../../config/conexaoBd')
+
 module.exports = function(app){
     app.get('/formulario', (req,res) => {
         res.render("form/cadastro")
     });
 
+
+    var conection = dbConnection()
     //rota para cadastro dos dados
-    app.post('/dados/salvar', (req,res) => {
+    app.post('/dados/salvar', function(req,res) {
         var dados = req.body;
-        res.send(dados)
+        conection.query('INSERT INTO pessoa SET ?',dados,(error, result)=> {        
+            if(error){
+                console.log(error)
+            }else{
+                res.redirect('/teste')
+            }
+            
+        })        
     });
 }
--- app/routes/teste.js
+++ app/routes/teste.js
@@ -1,16 +1,13 @@
-const res = require('express/lib/response');
-
 var dbConnection = require('../../config/conexaoBd')
 
-module.exports = function(app){ 
-    
+module.exports = function (app) {
+
     var conection = dbConnection()
 
-    app.get('/teste', function(req,res) {       
-   
-    conection.query( 'select * from pessoa' , function(error, result){
-        res.render("teste/teste", {pessoa : result })
-    })
+    app.get('/teste', function (req, res) {
+        conection.query('select * from pessoa', function (error, result) {
+            res.render("teste/teste", { pessoa: result })
+        })
 
     })
 }

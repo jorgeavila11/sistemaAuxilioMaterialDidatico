a0c038e06c4b58de356d28eb94e7659c708fee27
Jorge Avila <jorgeavila11@hotmail.com>
2022-04-18 21:03:31 -0300

    6.0 alterando dados funcionando

--- app/routes/alterarDados.js
+++ app/routes/alterarDados.js
@@ -1,30 +1,25 @@
 var dbConnection = require('../../config/conexaoBd')
 
-
-module.exports = function(app){
+module.exports = function (app) {
 
     var conection = dbConnection()
 
-    app.get('/alterar/:id', function(req,res) {
+    //rota preencher campo alterar
+    app.get('/alterar/:id', function (req, res) {
         const id = req.params.id
-        conection.query('select * from pessoa WHERE id = ?',[id], function (error, result) {
-            res.render("formAlterar/alterar", {dado : result })                      
-            //console.log({id})
+        conection.query('select * from pessoa WHERE id = ?', [id], function (error, result) {
+            res.render("formAlterar/alterar", { dado: result })
+            //console.log(result)
         })
     })
 
-    //var conection = dbConnection()
-
-    //rota para alterar dados
-    // app.post('/dados/alterar/:id', function(req,res) {
-    //     //const id = req.params.id
-    //     var dados = req.body
-    //     //res.render(dados)
-    //     conection.query('UPDATE pessoa SET nome = ?, email = ?, endereco = ? WHERE ID = ?', [id] ,function (error, result) {
-    //         res.redirect('/teste')
-    //     })
-    // });
-
+    app.post('/dados/alterar/:id', function(req,res) {
+        var dados = req.body
+        var id = req.params.id        
+        conection.query('UPDATE pessoa SET ? WHERE id = ?', [dados,id] ,function (error, result) {
+            res.redirect('/teste')
+        })        
+    });
 
 }
 
